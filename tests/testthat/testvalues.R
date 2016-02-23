94985a54572bddb8e50705734311d33555d297fc
Hovis  Anna E <hovisa@mathR.math.appstate.edu>
2016-02-22 20:27:35 -0500

    commit

--- tests/testthat/testvalues.R
+++ tests/testthat/testvalues.R
@@ -9,8 +9,4 @@ test_that("answers are correct",{
   expect_equal(RP(NA, 10), NA_integer_)
   expect_less_than(roll(1:20), 2*20 + 1)
   expect_more_than(roll(), 1)
-  expect_that(roll(dietype = 1:6), gives_warning("standard dice"))
-  expect_that(roll(dietype = 21:26), gives_warning("non-standard dice"))
-  expect_more_than(min(hair1958$X22), -0.0001)
-  expect_less_than(max(hair1958$X23), Inf)
 })
